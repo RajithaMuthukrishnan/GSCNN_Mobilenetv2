6c8f2da0b865f4405559d837bcd10d113ba18d12
Rajji7930 <57411032+Rajji7930@users.noreply.github.com>
2020-12-11 16:52:12 +0530

    Update and rename atrous_mobilenetv2.py to mobilenetv2.py

--- gated_shape_cnn/model/mobilenetv2.py
+++ gated_shape_cnn/model/mobilenetv2.py
@@ -11,21 +11,21 @@ def build_mobilenetv2():
         weights='imagenet',
         input_shape=[None, None, 3],)
 
-    atrous_mobilenetv2 = tf.keras.models.model_from_json(model.to_json())
-    atrous_mobilenetv2.set_weights(model.get_weights())
+    mobilenetv2 = tf.keras.models.model_from_json(model.to_json())
+    mobilenetv2.set_weights(model.get_weights())
         
-    for layer in atrous_mobilenetv2.layers:
+    for layer in mobilenetv2.layers:
         layer.trainable = False
     
-    return atrous_mobilenetv2
+    return mobilenetv2
 
 
 
 
-class AtrousMobilenetv2(tf.keras.models.Model):
+class Mobilenetv2(tf.keras.models.Model):
     def __init__(self, **kwargs):
         mobilenet = build_mobilenetv2()
-        super(AtrousMobilenetv2, self).__init__(inputs=mobilenet.inputs, outputs=mobilenet.outputs, **kwargs)
+        super(Mobilenetv2, self).__init__(inputs=mobilenet.inputs, outputs=mobilenet.outputs, **kwargs)
 
 
 if __name__ == '__main__':
